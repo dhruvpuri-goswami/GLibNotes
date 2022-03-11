68b0a78e10d91ebd8536b11b1344593be4d563c7
Dhruvpuri Goswami <92724441+DhruvpuriGoswami@users.noreply.github.com>
2022-03-11 08:37:56 +0530

    home

--- GLib/contact.php
+++ GLib/contact.php
@@ -1,10 +1,26 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+body {
+    width: 100%;
+    height: 100vh;
+    background-image: url("assests/images/contact.png");
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    overflow: hidden;
+}
+</style>
 
 <body>
     <div class="w3-top w3-margin">
@@ -13,7 +29,8 @@
                 <img src="logo.png" width="17%">
             </a>
             <div class="w3-display-topmiddle">
-                <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-white  w3-hover-border-black">Home</a>
+                <a href="index.php"
+                    class="w3-bar-item w3-button w3-hover-none w3-border-white  w3-hover-border-black">Home</a>
                 <a href="about.php"
                     class="w3-bar-item w3-button w3-hover-none w3-border-white w3-hover-border-black">About
                     Us</a>
@@ -27,23 +44,20 @@
             </div>
         </div>
     </div>
-    <form method="POST" action="" class="w3-containe w3-center" style="margin-top: 7rem;">
-        <h2>Get Any Notes For Free</h2>
-        <h4>Find Your Notes..</h4>
-        <input type="text" class="w3-margin-top w3-padding-16" style="height: 35px;padding-left: 10px;" width="100px"
-            placeholder="Find Your Notes...">
-        <input type="submit" class="w3-bar-item w3-button w3-teal w3-round" style="height: 39px;margin-bottom: 5px;"
-            value="GO" name="go">
-    </form>
+    <div class="w3-padding" style="margin-top: 7rem;margin-left:3rem;">
+        <div class="w3-margin-left" style="margin-left: 5rem;">
+            <h2>Contact Us</h2>
+            <hr>
+            <h4>
+                <p>If you have any query or suggestion about our site.</p><br>
+                <p>you can Contact Us on following E-mail.</p><br>
+                <p><b>Email ID : </b>glibnotes@gmail.com</p><br>
+            </h4>
+        </div>
+
+    </div>
+
 
 </body>
 
 </html>
-<?php
-    if(isset($_POST['go'])){
-        echo "<script>
-                alert('Please Login Yourself !!!');
-                window.location.href='login.php';
-            </script>";
-    }
-?>
